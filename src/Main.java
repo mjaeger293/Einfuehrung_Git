de155bb2ec07e2e61fa3a09a5a0e1692cb324162
mjaeger293 <mjaeger@htl-steyr.ac.at>
2020-11-11 11:47:35 +0100

    Änderung: Conflict wird erzwungen

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Guten Tag Herr Petko");
         System.out.println("\n");
-        System.out.println("Petko war hier.");
+        System.out.println("Petko ist hier.");
     }
 }
