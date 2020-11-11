74526359e6527aac871f2df91745f8f2c74a9074
pgyurdzh <pgyurdzh@htl-steyr.ac.at>
2020-11-11 11:30:59 +0100

    Änderungen:
    Ausgabe "Petko war hier." hinzugefügt.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Guten Tag Herr Petko");
+        System.out.println("\n");
+        System.out.println("Petko war hier.");
     }
 }
